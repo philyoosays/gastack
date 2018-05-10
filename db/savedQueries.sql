fc315ab457637b4f1c18fbaa3c2482d69cd15900
Phil Yoo <philyoo@ool-ad020634.dyn.optonline.net>
2018-05-10 11:19:48 -0400

    deliverable bug on hold

---
+++ db/savedQueries.sql
@@ -0,0 +1,47 @@
+-- What are people looking at?
+SELECT
+  users.fname,
+  users.lname,
+  users.username,
+  cohort.cohort,
+  posts.post_title
+FROM users JOIN cohort
+  ON users.cohortid = cohort.id
+JOIN views ON views.userid = users.id
+JOIN posts ON posts.id = views.postid
+ORDER BY views.date_created DESC;
+
+-- Who voted on what?
+SELECT
+  users.fname,
+  users.lname,
+  cohort.cohort,
+  posts.post_title,
+  commentvotes.vote
+FROM commentvotes JOIN users
+  ON users.id = commentvotes.userid
+JOIN cohort ON cohort.id = users.cohortid
+JOIN posts ON  posts.id = commentvotes.postid;
+
+-- Search History
+SELECT
+  searchhistory.id,
+  users.fname,
+  users.username,
+  searchhistory.search,
+  posts.post_title
+FROM searchhistory
+JOIN users ON users.id = searchhistory.userid
+JOIN posts ON searchhistory.resultpost = posts.id
+ORDER BY searchdate DESC;
+
+-- Sign ups
+SELECT
+  users.fname,
+  users.lname,
+  users.username,
+  users.email,
+  cohort.cohort
+FROM users JOIN cohort
+  ON users.cohortid = cohort.id
+ORDER BY date_created DESC;
